90b7d59f51162dc6e7c79b66319e1ced7bbf2fb7
ratulbd <ratulbracu@gmail.com>
2025-05-18 16:11:17 +0600

    new deployment with changes v62

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -8,7 +8,7 @@ export async function handler(event, context) {
 
   try {
     const data = JSON.parse(event.body);
-    const googleScriptURL = 'https://script.google.com/macros/s/AKfycbxbaG3zgGf4_-NfscTTbk424meqappL4jmTTx3DaIZAHpHWYQCR4aWWFo1PT87cx73Y/exec';
+    const googleScriptURL = 'https://script.google.com/macros/s/AKfycbyg6PzM32g7ZdAJIIPKZHNa2bxdHvKY-sgaHWqXJYDqHI8uqi2VNz4V6mouViFji7Z5/exec';
 
     const response = await fetch(googleScriptURL, {
       method: 'POST',
